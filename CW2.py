e6dfc9eb3d578f3df3bbabceb130972baf1819db
ArtemiiMazurkin <artemiim@nycstudents.net>
2026-02-04 08:05:23 -0500

    abstract

--- CW2.py
+++ CW2.py
@@ -1,24 +1,26 @@
 def setup():
-    size(400, 400, P3D)
-    background(220)
-    no_stroke()
-    directional_light(51, 102, 126, -1, 0, 0)
-    translate(20, 50, 0)
-    sphere(30)
-    
+    size(800, 800)
+    background(255,182,193)
+    no_fill()
+    stroke_weight(2)
+
 def draw():
-    fill(255)
-    stroke(0)
-    #arc(200,200,100,100,radians(90+45),radians(360+45),PIE)
-    #triangle(10,10,20,20,100,100)
-    #quad(13, 18, 25, 29, 47, 57, 102, 16)
-    fill(255,50,255)
-    begin_shape()
-    vertex(100,100)
-    fill(255,100,255)
-    vertex(150, 100+100*sqrt(3))
-    fill(255,0,255)
-    vertex(75, 50+50*sqrt(3))
-    vertex(200, 50+50*sqrt(3))
-    vertex(100, 100+100*sqrt(3))
-    end_shape(CLOSE)
+    translate(width / 2, height / 2)
+
+    r = frame_count * 0.5
+    angle = frame_count * 0.05
+
+    x = cos(angle) * r
+    y = sin(angle) * r
+
+    stroke(
+        100 + 100 * sin(angle),
+        100 + 100 * sin(angle + 2),
+        100 + 100 * sin(angle + 4),
+        80
+    )
+
+    ellipse(x, y, 40 + 20 * sin(angle * 2), 40 + 20 * cos(angle * 2))
+
+    if r > width:
+        no_loop()
