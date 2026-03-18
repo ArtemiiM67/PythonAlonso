85cb705f08a725e0e99e216e93914f0e0cafe282
ArtemiiMazurkin <artemiim@nycstudents.net>
2026-03-18 09:59:07 -0400

    fkfkf

--- sineOscilitations.py
+++ sineOscilitations.py
@@ -1,12 +1,14 @@
 angle = 15
 num_circles = 12  
 rotation = 0
+bounce_direction = 1  
+bounce_amount = 0
 
 def setup():
     size(510, 350)
 
 def draw():
-    global angle, rotation
+    global angle, rotation, bounce_amount, bounce_direction
     background(20, 20, 30, 50)  
     
     translate(width / 2, height / 2) 
@@ -14,8 +16,8 @@ def draw():
     
     for i in range(num_circles):
         theta = remap(i, 0, num_circles, 0, TWO_PI)
-        x = cos(theta) * 100  
-        y = sin(theta) * 100  
+        x = cos(theta) * (100 + bounce_amount)  
+        y = sin(theta) * (100 + bounce_amount)
         
         sin_value = sin(radians(angle + (i * 30)))
         circle_size = remap(sin_value, -1, 1, 30, 90)
@@ -35,3 +37,14 @@ def draw():
         
     if rotation >= 360:
         rotation = 0
+        
+    if frame_count % 240 == 0:
+        bounce()
+        bounce()
+        bounce()
+        
+def bounce():
+    global bounce_direction, bounce_amount
+    bounce_amount += 20 * bounce_direction
+    if bounce_amount > 50 or bounce_amount < 0:
+        bounce_direction *= -1
